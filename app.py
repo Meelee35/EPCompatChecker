1199e85887455909c0f9312beb87fb43e2c0c3d8
Meelee35 <mariobrosu22@gmail.com>
2025-05-30 09:23:12 +0100

    Implement button functionality for compatibility check and auto-detection; add .gitignore for PyInstaller

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
 import sys
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -17,8 +18,22 @@ class MainWindow(QMainWindow):
         self.resize(568, 288)
         self.setFixedSize(self.size())
         self.setWindowTitle("Explorerpatcher compatibility checker")
+
+        self.checkCompatibility = self.ui.findChild(QPushButton, "checkCompatibility")
+        self.autoDetect = self.ui.findChild(QPushButton, "autoDetect")
+
+        if self.checkCompatibility:
+            self.checkCompatibility.clicked.connect(self.checkCompat)
+
+        if self.autoDetect:
+            self.autoDetect.clicked.connect(self.autoDetecting)
+
+    def checkCompat(self):
+        print("Checking compatibility...")
         
-        
+    def autoDetecting(self):
+        print("Auto-detecting...")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
